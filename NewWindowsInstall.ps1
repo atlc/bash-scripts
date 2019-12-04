8d101e2544042866c2092bda839f1703af7a18c3
Andrew Lloyd Cartwright <andrewlloydcartwright+atlc@gmail.com>
2019-12-04 17:16:28 -0600

    Update NewWindowsInstall.ps1

--- NewWindowsInstall.ps1
+++ NewWindowsInstall.ps1
@@ -1,15 +1,17 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-choco install chromium firefox 7zip git nodejs gimp vscode procexp steam
+choco feature enable -n allowGlobalConfirmation
+choco install chromium firefox 7zip git nodejs gimp vscode procexp steam slack libreoffice
 
+Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux
 dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart
 dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart
 
 cd "C:\Users\$Env:UserName"
 Invoke-WebRequest -Uri https://aka.ms/wsl-ubuntu-1804 -OutFile Ubuntu.appx -UseBasicParsing
 Add-AppxPackage .\Ubuntu.appx
+.\Ubuntu.appx
 
 echo "`n`n`n"; Pause; echo "`n`n`n"
 
 wsl --set-version Ubuntu 2
 wsl --set-default-version 2
-
